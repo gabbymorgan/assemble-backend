7091cd1d27ce4271d11e4dd6e6f5f06e5fa428b2
William Pelton <wpelton@ambitenergy.com>
2019-09-01 15:13:08 -0500

    cors, nodemon, morgan

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
+const morgan = require("morgan");
 require("dotenv").config();
 const userRoutes = require('./routes/user');
 
@@ -10,10 +11,15 @@ const { DB_USER, DB_PASSWORD } = process.env;
 const port = 5000;
 const app = express();
 
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 //middleware
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(morgan());
 
 // routes
 app.use('/api/users', userRoutes);
